d2650bfb8dbf76a25b22cb415ce083dc92cfb3d7
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2021-05-09 19:08:04 +0100

    Update readability.c

--- pset2/readability.c
+++ pset2/readability.c
@@ -5,47 +5,88 @@
 #include <string.h>
 
 // Prototype or Function Declaration
-string count_letters(string a);
+int count_letters(string a);
+int count_words(string a);
+int count_sentences(string a);
 
 int main(void)
 {
     // Prompt user for a string with get_string
     string userText = get_string("Text: ");
-    // debug printf
-    printf("Text: %s\n", userText);
 
-    string letters = count_letters(userText);
+    // Functions
+    int letters = count_letters(userText);
+    int words = count_words(userText);
+    int sentences = count_sentences(userText);
 
-    // Print Grade output
+    // Average letters and sentences
+    double L = (double) letters * (double) 100 / (double) words;
+    double S = (double) sentences * (double) 100 / (double) words;
+
+    // Coleman-Liau index rounded to the nearest int
+    double index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    // Print Grade output:
+    // if grade > 16 output Grade16+
+    if (index > 16)
+    {
+        printf("Grade 16+\n");
+        return 0;
+    }
+    // if grade < 1 output Before Grade 1
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+        return 0;
+    }
+    else
+    {
+        printf("Grade %.f\n", index);
+    }
 }
 
-// Count number of letter
-string count_letters(string a) 
+// Count number of letters
+int count_letters(string a)
 {
-    // Use function declared
     int x = 0;
 
     for (int i = 0, n = strlen(a); i < n; i++)
+    {
+        if (islower(a[i]) || isupper(a[i]))
         {
-            if ((a[i] != '.' && a[i] != '!' && a[i] != '?')) 
-            {
-                x++;
-            }
-            
+            x++;
         }
-        printf("%i letter(s)\n", x);
-        return a;
+    }
+    
+    return x;
 }
 
-
-
 // Count number of words
+int count_words(string a)
+{
+    int y = 1;
+
+    for (int i = 0, n = strlen(a); i < n; i++)
+    {
+        if (a[i] == 32)
+        {
+            y++;
+        }
+    }
+    return y;
+}
 
 // Count number of sentences
+int count_sentences(string a)
+{
+    int z = 0;
 
-
-// Print output: Grade X(level computed with formula)
-
-// If grade > 16 output Grade16+
-
-// If grade < 1 output Before Grade 1
+    for (int i = 0, n = strlen(a); i < n; i++)
+    {
+        if ((a[i] == 33) || (a[i] == 46) || (a[i] == 63))
+        {
+            z++;
+        }
+    }
+    return z;
+}
